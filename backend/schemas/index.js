6eb9a0dcb8bcbb51cceac7abe80d1d4fa3e96b50
msebarjr <msebarjr@gmail.com>
2023-09-14 23:56:33 -0400

    Created all the schemas for application

---
+++ backend/schemas/index.js
@@ -0,0 +1,7 @@
+import user from './user'
+import post from './post'
+import comment from './comment'
+import postedBy from './postedBy'
+import like from './like'
+
+export const schemaTypes = [user, post, comment, postedBy, like]
---
+++ backend/schemas/post.js
@@ -0,0 +1,58 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'url',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        // Enables images to responsively adapt to different aspect ratios
+        hotspot: true,
+      },
+    },
+    {
+      name: 'userId',
+      title: 'UserID',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'like',
+      title: 'Like',
+      type: 'array',
+      of: [{type: 'like'}],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [{type: 'comment'}],
+    },
+  ],
+}
