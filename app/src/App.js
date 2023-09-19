05c3eb3b7fab3db9038ed51c6dff86347954773d
msebarjr <msebarjr@gmail.com>
2023-09-19 18:19:08 -0400

    Setup react router within app

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Screens
+import Login from './screens/Login';
+import Home from './screens/Home';
 
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello World</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+
+        {/* Everything besides the path /login will direct to Home component */}
+        <Route path='/*' element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
