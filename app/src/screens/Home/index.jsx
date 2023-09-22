5e5da44041ccf5ffe2a1d47b31c8acff56e676fa
msebarjr <msebarjr@gmail.com>
2023-09-22 00:18:18 -0400

    Created sidebar navigation links

--- app/src/screens/Home/index.jsx
+++ app/src/screens/Home/index.jsx
@@ -7,6 +7,7 @@ import { myConfiguredSanityClient } from '../../sanity/sanityClient'
 
 // Custom Components
 import Sidebar from '../../components/Sidebar'
+import Post from '../../components/Sidebar/Post'
 
 // Data
 import { userQuery } from '../../utils/sanityData'
@@ -18,6 +19,7 @@ import Logo from '../../assets/images/logo_dark.png'
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false)
   const [user, setUser] = useState(null)
+  const scrollRef = useRef(null)
 
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
 
@@ -29,9 +31,14 @@ const Home = () => {
       setUser(data[0])
     })
   }, [])
+
+  useEffect(() => {  
+    scrollRef.current.scrollTo(0, 0)
+  }, [])
+  
   
   // Opens and closes the sidebar
-  const sidebarClickHandler = () => {
+  const handleSidebarToggle = () => {
     setToggleSidebar(prevToggleState => !prevToggleState)
   }
 
@@ -41,22 +48,30 @@ const Home = () => {
         <Sidebar user={user && user}/>
       </div>
       <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className='cursor-pointer' onClick={sidebarClickHandler} />
-        <Link to='/'>
-          <img src={Logo} alt="Logo" className='w-28'/>
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="Logo" className='w-28'/>
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in animate-slide-out'>
-          <div className='absolute w-full flex justify-end items-center p-2'>
-            <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={sidebarClickHandler} />
-          </div>
-          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        <div className='p-4 w-full flex flex-row justify-between items-center shadow-md'>
+          <HiMenu fontSize={40} className='cursor-pointer' onClick={handleSidebarToggle} />
+          <Link to='/'>
+            <img src={Logo} alt="Logo" className='w-28'/>
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="Logo" className='w-16 rounded-full'/>
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in animate-slide-out'>
+            <div className='absolute w-full flex justify-end items-center p-2'>
+              <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={handleSidebarToggle} />
+            </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          {/* <Route path='/user-profile/:userId' element={<UserProfile />} /> */}
+          <Route path='/*' element={<Post user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   )
 }
