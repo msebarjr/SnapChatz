43ad088ad7a5bd83891bea7f745651a7b8a6ab11
msebarjr <msebarjr@gmail.com>
2023-09-23 23:22:49 -0400

    Connected home screen image feed to sanity client data

--- app/src/components/Feed/index.js
+++ app/src/components/Feed/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { myConfiguredSanityClient } from '../../sanity/sanityClient';
+
+// Data
+import { feedQuery, searchQuery } from '../../utils/sanityData';
+
+// Custom Components
+import Spinner from '../Spinner';
+import MasonryLayout from '../MasonryLayout';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    // If there is a specific search the categoryId will be present otherwise produces all images on the feed
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      myConfiguredSanityClient.fetch(query).then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
+    } else {
+      myConfiguredSanityClient.fetch(feedQuery).then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (isLoading)
+    return <Spinner message='We are finding new images for you!' />;
+
+  return <div>{posts && <MasonryLayout posts={posts} />}</div>;
 };
 
 export default Feed;
