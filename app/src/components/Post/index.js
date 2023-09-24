26b63743fd4e819be32679b36d7f4d6cd8cdb164
msebarjr <msebarjr@gmail.com>
2023-09-23 23:43:09 -0400

    Displayed feed on home screen with images

--- app/src/components/Post/index.js
+++ app/src/components/Post/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Post = () => {
-  return <div>Post</div>;
+// Sanity image url builder
+import { urlFor } from '../../client';
+
+const Post = ({ post: { postedBy, image, _id, destination } }) => {
+  return (
+    <div>
+      <img
+        src={urlFor(image).width(250).url()}
+        alt='user-post'
+        className='rounded-lg w-full'
+      />
+    </div>
+  );
 };
 
 export default Post;
